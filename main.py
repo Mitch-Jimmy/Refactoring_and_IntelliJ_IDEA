827a2e177310d85c8716346d3ffacf274d6b6941
Jimmy <tsaiwanghung@gmail.com>
2025-07-25 18:12:10 +0800

    Initial commit with refactored variable

---
+++ main.py
@@ -0,0 +1,24 @@
+count = 5
+foo = [ 0, 1, 2, 3, 4 ]
+
+# bigger variable
+m = 100
+bar = []
+
+for i in range(m):
+    bar.append(i)
+
+# function to count even numbers
+def countEvenNum(arr, size):
+    count = 0
+    for i in range(size):
+        if arr[i] % 2 == 0:
+            count += 1
+        else:
+            #do nothing
+            pass
+    return count
+
+# call the function
+print("The total number of even numbers in foo are: ", countEvenNum(foo, count))
+print("The total number of even numbers in bar are: ", countEvenNum(bar, m))
