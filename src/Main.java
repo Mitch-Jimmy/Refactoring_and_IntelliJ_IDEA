b4415c5a2b71d760e9251e68769a0b7ceae2fee2
tsaiw <tsaiwanghung@gmail.com>
2025-08-19 00:02:21 +0800

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,25 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main{
+    public static int countEvenNum(int[] arr, int size){
+        int count = 0;
+        for(int i = 0; i < size; i++){
+            if(arr[i] % 2 == 0){
+                count++;
+            }
         }
+        return count;
+    }
+
+    public static void main(String[] args){
+        int n = 5;
+        int[] foo = {0, 1, 2, 3, 4};
+
+        int m = 100;
+        int[] bar = new int[m];
+        for(int i = 0; i < m; i++){
+            bar[i] = i;
+        }
+
+        System.out.println("The total number of even numbers in foo is: " + countEvenNum(foo, n));
+        System.out.println("The total number of even numbers in bar is: " + countEvenNum(bar, m));
     }
 }
